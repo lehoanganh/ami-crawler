ba2838a121464a16721882a5c4f47004a36464cf
Hoang Anh Le <hoanganh.le@me.com>
2012-05-23 14:17:05 +0200

    mongoimport

---
+++ tools/mongoimport-script.rb
@@ -0,0 +1,67 @@
+# @author: me[at]lehoanganh[dot]de
+
+# used to import multiple json files and write each json in a single line
+
+# require 'rubygems'
+# require 'yajl'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+logger.info "Welcome!"
+logger.info "The script will import multiple json file in the current directory"
+logger.info "and write each json in a single line"
+
+# JSON Parser
+# parser = Yajl::Parser.new
+
+current_dir = File.dirname __FILE__
+
+# the file which contains multiple lines, each line is a json content
+final_list = File.expand_path(current_dir + "/final_list.txt")
+
+# already exist, than create a new one
+if File.exist? final_list
+  File.delete final_list
+  File.open(final_list,"w") { }
+end
+f = File.open(final_list,'a')
+
+# contains all file names in the current direct
+entries = Dir.entries current_dir
+
+# iterate
+entries.each do |file_name|
+  
+  # . and .. are not interesting
+  if file_name != "." && file_name != ".." && file_name.to_s.include?("json")
+    
+    logger.info "Parsing #{file_name}..."
+    
+    # temporary string
+    str = String.new
+    
+    # some json files contain nothing
+    if File.zero? file_name
+      str = "{ \"info\":\"Ohai for now is not supported by the system. Read in package_manager_info.json for more details\" }"  
+    else
+      # json file
+      json = File.open(file_name,"r")
+      
+      # read the json file
+      json.each {|line| str << line}
+       
+      # delete all new line characters
+      str = str.gsub("\n","")
+      
+      if str.include? "Ohai for now is not supported by the system"
+        str = "{ \"info\":\"Ohai for now is not supported by the system. Read in package_manager_info.json for more details\" }"
+      end  
+    end
+      
+    # write into the final list
+    f << str << "\n"
+    end
+end
+
+logger.info "Saving in final_list.txt..."
+logger.info "Done, bye!"
